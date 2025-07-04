3825f8a9bb716ca26966d824ef9d8b24b36b2537
cheforgodwin <cheforgodwin01@gmail.com>
2025-07-03 22:02:26 -0700

    added day19b

---
+++ day19b/nav.js
@@ -0,0 +1,8 @@
+ <script>
+    const toggleBtn = document.querySelector('nav-toggle');
+    const navLinks = document.querySelector('nav-links');
+
+    toggleBtn.addEventListener('click', () = {
+      navLinks.classList.toggle('show')
+    });
+  </script>
